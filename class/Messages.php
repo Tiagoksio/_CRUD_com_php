2775e3a2634af4af2d7fda7373dfb3a288abe7fd
tiagoksio <tiagoksio@gmail.com>
2021-10-25 06:38:18 -0300

    Update Messages.php
    add função insert

--- class/Messages.php
+++ class/Messages.php
@@ -1,6 +1,7 @@
 <?php
 class Messages {
     private $pdo;
+    public $res;
 
     public function __construct($dbname, $host, $user, $password) {
         try {
@@ -12,10 +13,28 @@ class Messages {
         }
     }
 
+    //SELECT
     public function getMessages () {
-        $res = array();
+        $res = array(); //Retornar o Array vazio caso não encontre mensagens;
         $cmd = $this -> pdo -> query("SELECT * FROM tb_mensagens ORDER BY nome DESC");
         $res = $cmd->fetchAll(PDO::FETCH_ASSOC);
         return $res;
+    }
+
+    //INSERT
+    public function setMessage ($nome, $sobrenome, $telefone, $email, $cidade, $assunto, $msg ) {
+
+        $cmd = $this -> pdo -> prepare("INSERT INTO tb_mensagens (nome, sobrenome, telefone, email, cidade, assunto, msg) VALUES (:nome, :sobrenome, :telefone, :email, :cidade, :assunto, :msg)");
+
+        $cmd -> bindValue(':nome', $nome);
+        $cmd -> bindValue(':sobrenome', $sobrenome);
+        $cmd -> bindValue(':telefone', $telefone);
+        $cmd -> bindValue(':email', $email);
+        $cmd -> bindValue(':cidade', $cidade);
+        $cmd -> bindValue(':assunto', $assunto);
+        $cmd -> bindValue(':msg', $msg);
+        $cmd -> execute();
+
+        return true;
     }
 }
