d512f2682e282af29e6dfb32d9dd0095258d7879
tiagoksio <tiagoksio@gmail.com>
2021-10-26 03:59:42 -0300

    add metodo de update

---
+++ crud/Messages.php
@@ -0,0 +1,62 @@
+<?php
+class Messages {
+    private $db_conn;
+
+    public function __construct($dbname, $host, $user, $password) {
+        try {
+            $this->db_conn = new PDO("mysql:dbname=".$dbname.";host:host=".$host, $user, $password );
+            $this->db_conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo "Ocorreu um erro com o banco de dados: ".$e->getMessage();
+        } catch (Exception $e) {
+            echo "Ocorreu um erro: ".$e->getMessage();
+        }
+    }
+
+    //SELECT
+    public function getMessages () {
+        $res = array(); //Retornar o Array vazio caso não encontre mensagens;
+        $cmd = $this -> db_conn -> query("SELECT * FROM tb_mensagens ORDER BY id DESC");
+        $res = $cmd->fetchAll(PDO::FETCH_ASSOC);
+        return $res;
+    }
+
+    //INSERT
+    public function setMessage ($nome, $sobrenome, $email, $telefone, $cidade, $assunto, $msg ) {
+
+        $cmd = $this -> db_conn -> prepare("INSERT INTO tb_mensagens (nome, sobrenome, email, telefone, cidade, assunto, msg) VALUES (:nome, :sobrenome, :email, :telefone, :cidade, :assunto, :msg)");
+
+        $cmd -> bindValue(':nome', $nome);
+        $cmd -> bindValue(':sobrenome', $sobrenome);
+        $cmd -> bindValue(':email', $email);
+        $cmd -> bindValue(':telefone', $telefone);
+        $cmd -> bindValue(':cidade', $cidade);
+        $cmd -> bindValue(':assunto', $assunto);
+        $cmd -> bindValue(':msg', $msg);
+        $cmd -> execute();
+
+        return true;
+    }
+
+    //DELETE
+    public function deleteMessage($id_msg) {
+        $cmd = $this -> db_conn -> prepare("DELETE FROM tb_mensagens WHERE id = :id");
+        $cmd -> bindValue(":id", $id_msg);
+        $cmd -> execute();
+    }
+
+    //UPDATE
+    public function updateMessage($id_msg, $nome, $sobrenome, $email, $telefone, $cidade, $assunto, $msg) {
+        $cmd = $this -> db_conn -> prepare("UPDATE tb_mensagens SET nome = :n, sobrenome = :sn, email = :e, telefone = :t, cidade = :c, assunto = :a, msg = :msg WHERE id = :id");
+
+        $cmd -> bindValue(':n', $nome);
+        $cmd -> bindValue(':sn', $sobrenome);
+        $cmd -> bindValue(':e', $email);
+        $cmd -> bindValue(':t', $telefone);
+        $cmd -> bindValue(':c', $cidade);
+        $cmd -> bindValue(':a', $assunto);
+        $cmd -> bindValue(':msg', $msg);
+        $cmd -> bindValue(':id', $id_msg);
+        $cmd -> execute();
+    }
+}
---
+++ template/header.php
@@ -0,0 +1,33 @@
+<header>
+    <div class="header">
+        <div class="sidebar_btn">
+            <img src="icons\menu.png" alt="login">
+            <span>Menu</span> 
+            </label>                    
+        </div>
+        <div class="logo">
+            <h1>
+                <a href="index.php">Logo</a>
+            </h1>
+        </div>
+        <div class="nav-icons">
+            <a href="#" class="search-btn">
+                <span>Pesquisar</span>
+                <img id="pesquisar" src="icons\004-loupe.png" alt="Pesquisar">
+            </a>
+            <a href="login.php" class="login-btn">
+                <span>Login</span>
+                <img src="icons\002-enter.png" alt="login">
+            </a>
+        </div>
+    </div>
+    <nav id="menu">
+        <ul>
+            <li><a href="index.php">Notícias</a></li>
+            <li><a href="selecao.php">Seleção</a></li>
+            <li><a href="#">Cadastro</a></li>
+            <li><a href="frm_fale_conosco.php">Fale Conosco</a></li>
+            <li><a href="#">Sobre</a></li>
+        </ul>                    
+    </nav>
+</header>
